a4ba1f2234fe6333f3c9d5fa5508a08245064c16
thando.mafela <thando.mafela@digital.hmrc.gov.uk>
2025-07-23 14:04:05 +0100

    DAC6-3439 - Added a Reporting Period error page.

---
+++ test/views/ReportingPeriodErrorViewSpec.scala
@@ -0,0 +1,43 @@
+package views
+
+import base.SpecBase
+import org.jsoup.Jsoup
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.i18n.{Lang, Messages}
+import play.api.mvc.{AnyContent, MessagesControllerComponents}
+import play.api.test.{FakeRequest, Injecting}
+import play.twirl.api.HtmlFormat
+import utils.ViewHelper
+import views.html.ReportingPeriodErrorView
+
+import java.time.{LocalDate, ZoneId}
+
+class ReportingPeriodErrorViewSpec extends SpecBase with GuiceOneAppPerSuite with Injecting with ViewHelper {
+
+  val view: ReportingPeriodErrorView = app.injector.instanceOf[ReportingPeriodErrorView]
+  val messagesControllerComponentsForView: MessagesControllerComponents = app.injector.instanceOf[MessagesControllerComponents]
+
+  implicit private val request: FakeRequest[AnyContent] = FakeRequest()
+  implicit private val messages: Messages = messagesControllerComponentsForView.messagesApi.preferred(Seq(Lang("en")))
+  val currentYear = LocalDate.now(ZoneId.of("Europe/London")).getYear
+
+  "InvalidMessageTypeErrorView" - {
+
+    "should render page components" in {
+      val renderedHtml: HtmlFormat.Appendable = view(currentYear)
+      lazy val doc = Jsoup.parse(renderedHtml.body)
+
+      val paragraphValues = Seq(
+        s"The ReportingPeriod element in your file must contain a date between 31 December 2014 and 31 December ${currentYear}.",
+        "Update the ReportingPeriod and upload the updated file."
+      )
+
+      getWindowTitle(doc) mustEqual "There is a problem with the reporting period in your file - Send a CRS or FATCA report - GOV.UK"
+      getPageHeading(doc) mustEqual "There is a problem with the reporting period in your file"
+      validateAllParaValues(getAllParagraph(doc).text(), paragraphValues)
+    }
+  }
+
+  private def validateAllParaValues(allParaValues: String, paraValues: Seq[String]): Unit =
+    paraValues.foreach(allParaValues must include(_))
+}
