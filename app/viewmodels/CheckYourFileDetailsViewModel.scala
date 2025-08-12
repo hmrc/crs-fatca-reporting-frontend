7249ead2607e3a78addb21d45f657e53b5c37874
matthewharris1 <matthew.harris@digital.hmrc.gov.uk>
2025-08-12 16:55:08 +0100

    DAC6-3165 | Added new Check file details page and specs

---
+++ app/viewmodels/CheckYourFileDetailsViewModel.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels
+
+import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist._
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.Text
+
+object CheckYourFileDetailsViewModel {
+
+  def getYourFileDetailsRows()(implicit messages: Messages): SummaryList = {
+    SummaryList(
+      rows = Seq(
+        SummaryListRow(
+          key = Key(content = Text(messages("checkYourFileDetails.fileId.key"))),
+          value = Value(content = Text("MyFATCAReportMessageRefId234567890LONGONGLONGLONGLONG")),
+          classes = "no-border-bottom"
+        ),
+        SummaryListRow(
+          key = Key(content = Text(messages("checkYourFileDetails.reportingRegime.key"))),
+          value = Value(content = Text("CRS")),
+          classes = "no-border-bottom"
+        ),
+        SummaryListRow(
+          key = Key(content = Text(messages("FI ID (SendingCompanyN)"))),
+          value = Value(content = Text("ABC00000123")),
+          classes = "no-border-bottom"
+        ),
+        SummaryListRow(
+          key = Key(content = Text(messages("Financial institution (ReportingFIName)"))),
+          value = Value(content = Text("EFG Bank plc")),
+          classes = "no-border-bottom"
+        ),
+        SummaryListRow(
+          key = Key(content = Text(messages("File information"))),
+          value = Value(content = Text("New information")),
+          actions = Some(
+            Actions(
+              items = Seq(
+                ActionItem(
+                  href = "#",
+                  content = Text(messages("checkYourFileDetails.fileInformation.change")),
+                  visuallyHiddenText = Some(messages("checkYourFileDetails.fileInformation.change"))
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+  }
+}
---
+++ test/controllers/CheckYourFileDetailsControllerSpec.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.SpecBase
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import views.html.CheckYourFileDetailsView
+import viewmodels.CheckYourFileDetailsViewModel
+import org.scalatest.matchers.must.Matchers
+
+class CheckYourFileDetailsControllerSpec extends SpecBase with Matchers {
+
+  "CheckYourFileDetails Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.CheckYourFileDetailsController.onPageLoad().url)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[CheckYourFileDetailsView]
+
+        val messagesApi = messages(application)
+        val expectedSummaryList = CheckYourFileDetailsViewModel.getYourFileDetailsRows()(messagesApi)
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(expectedSummaryList, "Placeholder Name")(request, messagesApi).toString
+      }
+    }
+  }
+}
