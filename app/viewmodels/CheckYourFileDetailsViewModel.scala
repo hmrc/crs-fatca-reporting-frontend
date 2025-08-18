25a597dfdc9403bba373f8aab33f92a2516a87ab
matthewharris1 <matthew.harris@digital.hmrc.gov.uk>
2025-08-18 08:19:43 +0100

    DAC6-3165 | Re added remove label from merging main

--- app/viewmodels/CheckYourFileDetailsViewModel.scala
+++ app/viewmodels/CheckYourFileDetailsViewModel.scala
@@ -38,11 +38,12 @@ object CheckYourFileDetailsViewModel {
           classes = "no-border-bottom"
         ),
         SummaryListRow(
+          key = Key(content = Text(messages("FI ID (SendingCompanyIN)"))),
           value = Value(content = Text("ABC00000123")),
           classes = "no-border-bottom"
         ),
         SummaryListRow(
-          key = Key(content = Text(messages("Financial institution (ReportingFI Name)"))),
+          key = Key(content = Text(messages("checkYourFileDetails.financialInstitution.key"))),
           value = Value(content = Text("EFG Bank plc")),
           classes = "no-border-bottom"
         ),
