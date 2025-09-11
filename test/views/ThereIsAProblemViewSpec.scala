0c899472116148c3c66a59e106afe7cde78d96c3
matthewharris1 <matthew.harris@digital.hmrc.gov.uk>
2025-09-11 10:27:45 +0100

    DAC6-3610 | Created 2 new problem views with specs

---
+++ test/views/ThereIsAProblemViewSpec.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views
+
+import base.SpecBase
+import org.jsoup.Jsoup
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.i18n.{Lang, Messages}
+import play.api.mvc.{AnyContent, MessagesControllerComponents}
+import play.api.test.{FakeRequest, Injecting}
+import play.twirl.api.HtmlFormat
+import utils.ViewHelper
+import views.html.ThereIsAProblemView
+
+class ThereIsAProblemViewSpec extends SpecBase with GuiceOneAppPerSuite with Injecting with ViewHelper {
+
+  val view1: ThereIsAProblemView                                              = app.injector.instanceOf[ThereIsAProblemView]
+  val messagesControllerComponentsForView: MessagesControllerComponents = app.injector.instanceOf[MessagesControllerComponents]
+
+  implicit private val request: FakeRequest[AnyContent] = FakeRequest()
+  implicit private val messages: Messages               = messagesControllerComponentsForView.messagesApi.preferred(Seq(Lang("en")))
+
+  "ThereIsAProblemView" - {
+    "should render page components" in {
+      val renderedHtml: HtmlFormat.Appendable =
+        view1()
+      lazy val doc = Jsoup.parse(renderedHtml.body)
+      val paragraphValues = Seq(
+        "Try again later.",
+        "You can email aeoi.enquiries@hmrc.gov.uk if you need to contact someone about your CRS or FATCA reporting."
+      )
+
+      getWindowTitle(doc) must include("Sorry, there is a problem with the service")
+      getPageHeading(doc) mustEqual "Sorry, there is a problem with the service"
+      validateAllParaValues(getAllParagraph(doc).text(), paragraphValues)
+    }
+  }
+
+}
