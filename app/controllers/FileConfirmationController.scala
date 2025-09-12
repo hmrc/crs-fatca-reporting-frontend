849415c005a7d056fb3ba0ad02e7c5723c8ac0e2
jayandran-Sampath <24522469+jayandran-Sampath@users.noreply.github.com>
2025-09-12 14:42:01 +0100

    DAC6-3238 | initial commit with controller & view

---
+++ app/controllers/FileConfirmationController.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.actions.*
+import models.fileDetails.FileDetails
+
+import javax.inject.Inject
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import utils.DateTimeFormats
+import utils.DateTimeFormats.dateFormatterForFileConfirmation
+import viewmodels.FileConfirmationViewModel
+import views.html.FileConfirmationView
+
+import java.time.LocalDateTime
+
+class FileConfirmationController @Inject() (
+  override val messagesApi: MessagesApi,
+  identify: IdentifierAction,
+  getData: DataRetrievalAction,
+  requireData: DataRequiredAction,
+  val controllerComponents: MessagesControllerComponents,
+  view: FileConfirmationView
+) extends FrontendBaseController
+    with I18nSupport {
+
+  def onPageLoad: Action[AnyContent] = (identify andThen getData) {
+    implicit request =>
+      val submittedTime = LocalDateTime.parse("2025-09-12T12:01:00")
+      val date = submittedTime.format(dateFormatterForFileConfirmation())
+      val time = DateTimeFormats.formatTimeForFileConfirmation(submittedTime)
+      val fileDetails = FileDetails("name.xml", "c-8-new-f-va","CRS", "EFG Bank plc", "New information", submittedTime, LocalDateTime.now())
+      val fileSummary = FileConfirmationViewModel.getSummaryRows(fileDetails)
+//      val paraContent = FileConfirmationViewModel.getEmailParagraphForNonFI("user1@email.com",Some("user2@email.com"),"fi1@email.com",Some("f12@email.com"))
+//      val paraContent = FileConfirmationViewModel.getEmailParagraphForNonFI("user1@email.com",Some("user2@email.com"),"fi1@email.com",None)
+//      val paraContent = FileConfirmationViewModel.getEmailParagraphForNonFI("user1@email.com",None,"fi1@email.com",Some("f12@email.com"))
+//      val paraContent = FileConfirmationViewModel.getEmailParagraphForNonFI("user1@email.com",None,"fi1@email.com",None)
+//      val paraContent = FileConfirmationViewModel.getEmailParagraphForFI("user1@email.com",Some("user2@email.com"))
+      val paraContent = FileConfirmationViewModel.getEmailParagraphForFI("user1@email.com",None)
+      Ok(view(fileSummary,paraContent,date,time,true))
+  }
+}
---
+++ app/viewmodels/FileConfirmationViewModel.scala
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels
+
+import models.fileDetails.FileDetails
+import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.Aliases.{Text, Value}
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.HtmlContent
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.{Key, SummaryList, SummaryListRow}
+
+object FileConfirmationViewModel {
+
+  def getSummaryRows(receivedFileDetails: FileDetails)(implicit messages: Messages): SummaryList =
+    SummaryList(
+      rows = Seq(
+        SummaryListRow(
+          key = Key(
+            content = Text(messages("fileConfirmation.messageRefId.key")),
+            classes = "govuk-file-confirmation__key"
+          ),
+          value = Value(content = Text(receivedFileDetails.messageRefId))
+        ),
+        SummaryListRow(
+          key = Key(
+            content = Text(messages("fileConfirmation.messageType.key")),
+            classes = "govuk-file-confirmation__key"
+          ),
+          value = Value(content = Text(receivedFileDetails.messageType))
+        ),SummaryListRow(
+          key = Key(
+            content = Text(messages("fileConfirmation.reportingFIName.key")),
+            classes = "govuk-file-confirmation__key"
+          ),
+          value = Value(content = Text(receivedFileDetails.reportingEntityName))
+        ),SummaryListRow(
+          key = Key(
+            content = Text(messages("fileConfirmation.fileInformation.key")),
+            classes = "govuk-file-confirmation__key"
+          ),
+          value = Value(content = Text(receivedFileDetails.fileInformation))
+        )
+      )
+    )
+
+  def getEmailParagraphForNonFI(regFirstEmail: String, regSecondEmail: Option[String],
+                                fiFirstEmail: String, fiSecondEmail: Option[String])
+                               (implicit messages: Messages): String = {
+    (regSecondEmail, fiSecondEmail) match {
+      case (Some(regSecEmail), Some(fiSecEmail)) => s"$regFirstEmail, $regSecEmail, $fiFirstEmail and $fiSecEmail"
+      case (Some(regSecEmail), None) => s"$regFirstEmail, $regSecEmail and $fiFirstEmail"
+      case (None, Some(fiSecEmail)) => s"$regFirstEmail, $fiFirstEmail and $fiSecEmail"
+      case (None, None) => s"$regFirstEmail and $fiFirstEmail"
+    }
+  }
+
+  def getEmailParagraphForFI(regFirstEmail: String, regSecondEmail: Option[String])
+                               (implicit messages: Messages): String = {
+    regSecondEmail match {
+      case Some(regSecEmail) => s"$regFirstEmail and $regSecEmail"
+      case None => s"$regFirstEmail"
+    }
+  }
+
+}
