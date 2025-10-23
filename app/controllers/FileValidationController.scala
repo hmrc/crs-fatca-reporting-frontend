e455e22f0bdd8dbb69948a2f7f95bd308eaf03cf
matthewharris1 <matthew.harris@digital.hmrc.gov.uk>
2025-10-23 17:21:57 +0100

    DAC6-3770 | Scalafmt

--- app/controllers/FileValidationController.scala
+++ app/controllers/FileValidationController.scala
@@ -20,7 +20,7 @@ import connectors.{UpscanConnector, ValidationConnector}
 import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
 import models.requests.DataRequest
 import models.upscan.*
-import models.{FIIDNotMatchingError, SchemaValidationErrors, IncorrectMessageTypeError, ReportingPeriodError, UserAnswers, ValidatedFileData}
+import models.{FIIDNotMatchingError, IncorrectMessageTypeError, ReportingPeriodError, SchemaValidationErrors, UserAnswers, ValidatedFileData}
 import pages.*
 import play.api.Logging
 import play.api.i18n.{I18nSupport, MessagesApi}
