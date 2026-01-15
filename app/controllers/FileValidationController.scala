9d508621213f4dce7da89a29820d5e1570c57ab1
matthewharris1 <matthew.harris@digital.hmrc.gov.uk>
2026-01-15 16:13:30 +0000

    DAC6-4064 | added encoded char check for ""

--- app/controllers/FileValidationController.scala
+++ app/controllers/FileValidationController.scala
@@ -63,6 +63,7 @@ class FileValidationController @Inject() (
 
   private val maxFileNameLength         = 100
   private val disallowedCharactersRegex = "[<>:\"'&/\\\\|?*]".r
+  private val disallowedEncodedCharacters = "%22"
 
   def onPageLoad(): Action[AnyContent] = (identify andThen getData andThen requireData).async {
     implicit request =>
@@ -203,7 +204,7 @@ class FileValidationController @Inject() (
     fileName.length > maxFileNameLength
 
   private def isDisallowedCharactersPresent(fileName: String): Boolean =
-    disallowedCharactersRegex.findFirstIn(fileName).isDefined
+    disallowedCharactersRegex.findFirstIn(fileName).isDefined  || fileName.contains(disallowedEncodedCharacters)
 
   private def navigateToErrorPage(uploadId: UploadId, errorMessage: String): Future[Result] =
     Future.successful(
