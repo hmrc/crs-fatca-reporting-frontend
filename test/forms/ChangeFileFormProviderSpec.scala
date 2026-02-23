b83247784af65fe1e841f5b64c8b08468d9c2090
Carl Thompson <carl.thompson@opencastsoftware.com>
2026-02-23 09:54:52 +0000

    DAC6-4104 | scalafmt

--- test/forms/ChangeFileFormProviderSpec.scala
+++ test/forms/ChangeFileFormProviderSpec.scala
@@ -22,7 +22,7 @@ import play.api.data.FormError
 class ChangeFileFormProviderSpec extends BooleanFieldBehaviours {
 
   val requiredKey = "changeFile.error.required"
-  val invalidKey = "error.boolean"
+  val invalidKey  = "error.boolean"
 
   val form = new ChangeFileFormProvider()()
 
