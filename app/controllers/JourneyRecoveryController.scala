73ac5db6280c0a6c9fa90cf8c252b009a33d9b90
Carl Thompson <carl.thompson@opencastsoftware.com>
2026-03-10 10:02:54 +0000

    DAC6-3838 | refactor JRController to use the designed problem page, add /report/ to the url

--- app/controllers/JourneyRecoveryController.scala
+++ app/controllers/JourneyRecoveryController.scala
@@ -20,39 +20,22 @@ import controllers.actions.IdentifierAction
 import play.api.Logging
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import uk.gov.hmrc.play.bootstrap.binders.RedirectUrl._
-import uk.gov.hmrc.play.bootstrap.binders._
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import views.html.{JourneyRecoveryContinueView, JourneyRecoveryStartAgainView}
+import views.html.ThereIsAProblemView
 
 import javax.inject.Inject
 
 class JourneyRecoveryController @Inject() (
   val controllerComponents: MessagesControllerComponents,
   identify: IdentifierAction,
-  continueView: JourneyRecoveryContinueView,
-  startAgainView: JourneyRecoveryStartAgainView
+  thereIsAProblemView: ThereIsAProblemView
 ) extends FrontendBaseController
     with I18nSupport
     with Logging {
 
-  def onPageLoad(continueUrl: Option[RedirectUrl] = None): Action[AnyContent] = identify {
+  def onPageLoad(): Action[AnyContent] = identify {
     implicit request =>
-      val safeUrl: Option[String] = continueUrl.flatMap {
-        unsafeUrl =>
-          unsafeUrl.getEither(OnlyRelative) match {
-            case Right(safeUrl) =>
-              Some(safeUrl.url)
-            case Left(message) =>
-              logger.info(message)
-              None
-          }
-      }
-
-      safeUrl
-        .map(
-          url => Ok(continueView(url))
-        )
-        .getOrElse(Ok(startAgainView()))
+      logger.error("Something went wrong")
+      Ok(thereIsAProblemView())
   }
 }
