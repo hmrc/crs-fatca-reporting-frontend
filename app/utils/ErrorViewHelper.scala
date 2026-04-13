4b315246b62c0cc74f9d4d61c0dab0c01fb78d05
Jayandran Sampath <24522469+jayandran-Sampath@users.noreply.github.com>
2026-04-13 11:58:51 +0100

    Dac6 3790 - Xml Schema error message & helper methods (#123)
    * DAC6-3790 | initial messages
    
    * DAC6-3790 | add sendingCompanyIN messages
    
    * DAC6-3790 | add message for OECD version attribute
    
    * DAC6-3790 | handle CorrMessageRefId message
    
    * DAC6-3790 | handle reportingPeriod & timestamp
    
    * DAC6-3790 | missing session
    
    * DAC6-3790 | add IN message
    
    * DAC6-3790 | update reporting period message
    
    * DAC6-3790 | add message for IN issuedBy invalid attribute value
    
    * DAC6-3790 | handle IN element INType attribute
    
    * DAC6-3790 | fix css for reportingPeriod
    
    * DAC6-3790 | handle enum arg messages
    
    * DAC6-3790 | fix section typo
    
    * DAC6-3790 | handle DocSpec element
    
    * DAC6-3790 | add bool argument
    
    * DAC6-3790 | handle TIN argument
    
    * DAC6-3790 | add name element message
    
    * DAC6-3790 | add nationality element message
    
    * DAC6-3790 | add city messages
    
    * DAC6-3790 | add Decimal invalid message
    
    * DAC6-3790 | add fraction message
    
    * DAC6-3790 | add iso arg message
    
    * DAC6-3790 | add number invalid message
    
    * DAC6-3790 | add poolreport exclusion message
    
    * DAC6-3790 | add unknown element message
    
    * DAC6-3790 | add unknown arg message
    
    * DAC6-3790 | add unknown child message
    
    * DAC6-3790 | handle messagerefId max
    
    * DAC6-3790 | add invalid timestamp message
    
    * DAC6-3790 | add country info empty message
    
    * DAC6-3790 | add addressfix message
    
    * DAC6-3790 | use correct error messages
    
    * DAC6-3790 | add unit test for ErrorViewHelper
    
    * DAC6-3790 | fix max errors
    
    * DAC6-3790 | handle accountHolder child missing
    
    * DAC6-3790 | add crs oecd min message
    
    * DAC6-3790 | refactor
    
    * DAC6-3790 | update unknown elem msg
    
    * DAC6-3790 | update version min message
    
    * DAC6-3790 | fix bullet list with number bug

--- app/utils/ErrorViewHelper.scala
+++ app/utils/ErrorViewHelper.scala
@@ -16,21 +16,85 @@
 
 package utils
 
-import models.GenericError
+import models.{GenericError, Message}
 import play.api.i18n.Messages
 import uk.gov.hmrc.govukfrontend.views.Aliases.Text
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.HtmlContent
 import uk.gov.hmrc.govukfrontend.views.viewmodels.table.TableRow
+import utils.ReportingConstants.MAX_DATA_ERRORS
 
 import javax.inject.Inject
 
 class ErrorViewHelper @Inject() () {
 
   def generateTable(error: Seq[GenericError])(implicit messages: Messages): Seq[Seq[TableRow]] =
-    error.map {
+    error.take(MAX_DATA_ERRORS).map {
       er =>
-        Seq(
-          TableRow(content = Text(er.lineNumber.toString), classes = "govuk-table__cell--numeric", attributes = Map("id" -> s"lineNumber_${er.lineNumber}")),
-          TableRow(content = Text(messages(er.message.messageKey, er.message.args: _*)), attributes = Map("id" -> s"errorMessage_${er.lineNumber}"))
-        )
+        er.message.messageKey match {
+          case "xml.elem.reportingPeriod.invalid" => invalidReportingPeriod(er.lineNumber)
+          case "xml.elem.DocRefId.max"            => invalidDocRef(er.lineNumber)
+          case "xml.elem.messageRefId.max"        => messageRefId(er.lineNumber)
+          case "xml.elem.unknown"                 => unknownElem(er.lineNumber, er.message)
+          case _ =>
+            Seq(
+              TableRow(content = Text(er.lineNumber.toString),
+                       classes = "govuk-table__cell--numeric",
+                       attributes = Map("id" -> s"lineNumber_${er.lineNumber}")
+              ),
+              TableRow(content = Text(messages(er.message.messageKey, er.message.args: _*)), attributes = Map("id" -> s"errorMessage_${er.lineNumber}"))
+            )
+        }
+
     }
+
+  private def invalidReportingPeriod(lineNumber: Int)(implicit messages: Messages) = errorRows(lineNumber, "xml.elem.reportingPeriod.invalid", 3)
+
+  private def invalidDocRef(lineNumber: Int)(implicit messages: Messages) = errorRows(lineNumber, "xml.elem.DocRefId.max", 3)
+
+  private def messageRefId(lineNumber: Int)(implicit messages: Messages) = errorRows(lineNumber, "xml.elem.messageRefId.max", 7)
+
+  private def unknownElem(lineNumber: Int, m: Message)(implicit messages: Messages) = {
+    val htmlContent =
+      s"""
+                          <p class="govuk-body govuk-!-margin-bottom-1">${messages(m.messageKey, m.args: _*)}</p>
+                          <ul class="govuk-list govuk-list--bullet govuk-!-margin-bottom-0">
+                            <li>${messages("xml.elem.unknown.li1")}</li>
+                            <li>${messages("xml.elem.unknown.li2")}</li>
+                          </ul>
+                          """
+    Seq(
+      TableRow(content = Text(lineNumber.toString), classes = "govuk-table__cell--numeric", attributes = Map("id" -> s"lineNumber_$lineNumber")),
+      TableRow(content = HtmlContent(htmlContent), attributes = Map("id" -> s"errorMessage_$lineNumber"))
+    )
+  }
+
+  private def errorRows(lineNumber: Int, messageTag: String, listCount: Int)(implicit messages: Messages): Seq[TableRow] = {
+
+    val listItemsHtml = (1 to listCount)
+      .map {
+        index => s"<li>${messages(s"$messageTag.li$index")}</li>"
+      }
+      .mkString("\n")
+
+    val htmlContent =
+      s"""
+         |<p class="govuk-body govuk-!-margin-bottom-1">${messages(messageTag)}</p>
+         |<ul class="govuk-list govuk-list--bullet">
+         |  $listItemsHtml
+         |</ul>
+         |<p class="govuk-body govuk-!-margin-bottom-0">${messages(s"$messageTag.p2")}</p>
+         |""".stripMargin
+
+    Seq(
+      TableRow(
+        content = Text(lineNumber.toString),
+        classes = "govuk-table__cell--numeric",
+        attributes = Map("id" -> s"lineNumber_$lineNumber")
+      ),
+      TableRow(
+        content = HtmlContent(htmlContent),
+        attributes = Map("id" -> s"errorMessage_$lineNumber")
+      )
+    )
+  }
 }
